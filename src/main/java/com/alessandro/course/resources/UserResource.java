8312f12af3fefaf8495d3e8a6271e9fc90de8609
Alessandro Vaiz <alessandrovaiz@gmail.com>
2020-07-01 20:58:35 -0300

    Service layer, component registration

--- src/main/java/com/alessandro/course/resources/UserResource.java
+++ src/main/java/com/alessandro/course/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.alessandro.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alessandro.course.entities.User;
+import com.alessandro.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping // define função como tipo GET do http
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L,"Joao","joao@joao.com","123456789","1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
 	
 }
